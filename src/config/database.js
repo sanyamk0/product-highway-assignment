3cb29d2745d903ceb476eaefd27ba5bbe6d2a884
sanyamk0 <sanyamkumar002@gmail.com>
2024-11-13 13:46:36 +0530

    rename variables

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
